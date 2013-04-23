e901256897197f89229a4b3d4fae5da901645675
Bob Nisco <BobNisco@gmail.com>
2013-04-23 11:51:21 -0400

    Some fake, but real looking data for presentation

--- src/fote/FOTE.java
+++ src/fote/FOTE.java
@@ -8,6 +8,8 @@ import fote.entry.Vote;
 import fote.gui.Login;
 import fote.gui.MainFrame;
 import fote.util.MongoHelper;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 /**
@@ -37,32 +39,32 @@ public class FOTE {
         FOTE.mf = mf;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // Set the DB to fote 
         MongoHelper.setDB("fote");
-        // add user to logon...
+        // add some fake data
         MongoHelper.getCollection("users").drop();
         MongoHelper.getCollection("votes").drop();
-        MongoHelper.save(new User("Test", "Name", "admin", "admin"), "users");
-        MongoHelper.save(new User("Test brah2", "Name", "sadmin", "sadmin"), "users");
+        MongoHelper.save(new User("Bob", "Nisco", "robert.nisco1@marist.edu", "password123"), "users");
+        MongoHelper.save(new User("Jason", "Parraga", "jason.parraga1@marist.edu", "password123"), "users");
+        MongoHelper.save(new User("Evan", "Van Dam", "Evan.VanDam1@marist.edu", "password123"), "users");
         MongoHelper.getCollection("proposals").drop();
         ArrayList<String> options = new ArrayList<String>();
-        options.add("yes");
-        options.add("no");
+        options.add("Yes");
+        options.add("No");
         MongoHelper.getCollection("comments").drop();
-        MongoHelper.save(new Comment("swag", new Integer(0)), "comments");
-        MongoHelper.save(new Comment("derp", new Integer(1)), "comments");
         ArrayList<Integer> comments = new ArrayList<Integer>();
         comments.add(new Integer(0));
         comments.add(new Integer(1));
-        MongoHelper.save(new Proposal(new Date(), "Test subject", "Test description", 5, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
-        MongoHelper.save(new Proposal(new Date(), "Test subject1", "Test description", 4, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
-        MongoHelper.save(new Proposal(new Date(), "Test subject2", "Test description", 3, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
-        MongoHelper.save(new Proposal(new Date(), "Test subject3", "Test description", 2, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
-        MongoHelper.save(new Proposal(new Date(), "Test subject4", "Test description", 1, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
-        MongoHelper.save(new Proposal(new Date(), "Test subject5", "Test description", 1, new Integer(0), options, new ArrayList<Vote>(), new ArrayList<Integer>(), new ArrayList<String>()), "proposals");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        MongoHelper.save(new Proposal(sdf.parse("2013/08/29"), "Build a New Cafeteria?", "We have listened to the thoughts of students and realized that a new cafeteria would be beneficial.",
+                2, new Integer(0), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
+        MongoHelper.save(new Proposal(sdf.parse("2013/01/01"), "Renovate 3rd Floor of Lowell Thomas", "We recently renovated the basement and second floor, but we should also renovate the third floor.",
+                1, new Integer(1), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
+        MongoHelper.save(new Proposal(sdf.parse("2014/05/01"), "Build a Parking Garage", "Build a parking garage where the current McCann Center parking lot is. Would improve parking area for commuters and during game days.",
+                0, new Integer(2), options, new ArrayList<Vote>(), comments, new ArrayList<String>()), "proposals");
         MongoHelper.getCollection("suggestions").drop();
-        MongoHelper.save(new Suggestion("test subject", "test description", 0, new ArrayList<Integer>(), new ArrayList<String>()), "suggestions");
+        MongoHelper.save(new Suggestion("Purchase Solar Panels", "Put solar panels on top of Hancock Center to be more eco-friendly", 0, new ArrayList<Integer>(), new ArrayList<String>()), "suggestions");
         setMainFrame(new MainFrame());
         new Login(FOTE.getMainFrame(), true).setVisible(true);
     }
