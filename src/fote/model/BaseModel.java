6d8de891a1d5a499c9c6a2cb15f264c23033fbb6
Evan Van Dam <ecv92@yahoo.com>
2013-05-08 11:52:58 -0400

    documented baseModel

--- src/fote/model/BaseModel.java
+++ src/fote/model/BaseModel.java
@@ -5,7 +5,9 @@ import fote.util.MongoHelper;
 import java.util.ArrayList;
 
 /**
- *
+ * This class allows for simpler and uniform querying methods by calling
+ * its subclasses which do not require the mongo collection name.
+ * 
  * @author Bob Nisco
  */
 public abstract class BaseModel {
@@ -13,16 +15,32 @@ public abstract class BaseModel {
     private String document;
     private Class clss;
     
+    /**
+     * 
+     * @param doc The name of the Mongo collection
+     * @param cls The class type that results will be found as
+     */
     public BaseModel(String doc, Class cls) {
         this.document = doc;
         this.clss = cls;
     }
 
+    /**
+     * 
+     * @param queryString JSON representation of the object
+     * @return an Iterable set of matching entries
+     */
     public Iterable<Entry> query(String queryString) {
         Iterable<Entry> resultSet = MongoHelper.query(queryString, clss, document);
         return resultSet;
     }
     
+    /**
+     * 
+     * @param queryString JSON representation of the object
+     * @param numbers limit the results set
+     * @return an iterable result set of matching entries
+     */
     public Iterable<Entry> query(String queryString, ArrayList<Integer> numbers) {
         Iterable<Entry> resultSet = MongoHelper.query(queryString, numbers, clss, document);
         return resultSet;
