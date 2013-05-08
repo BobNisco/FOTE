cc07c7ac26675a7164cb7ca469da0775ef81a6bd
Evan Van Dam <ecv92@yahoo.com>
2013-05-08 11:59:02 -0400

    suggestionLogic documented

--- src/fote/controller/SuggestionLogic.java
+++ src/fote/controller/SuggestionLogic.java
@@ -10,10 +10,17 @@ import fote.util.MongoHelper;
 import java.util.ArrayList;
 
 /**
- *
+ * This class handles logic for suggestions, such as creating, 
+ * updating, and deleting.
  * @author Bob Nisco
  */
 public class SuggestionLogic {
+    /**
+     * Create a suggestion and attempt to save it to Mongo
+     * @param subject
+     * @param description
+     * @return if the entry was successfully saved to Mongo
+     */
     public static boolean createSuggestion(String subject, String description) {
         Suggestion suggestion = new Suggestion();
         suggestion.setSubject(subject);
@@ -30,7 +37,11 @@ public class SuggestionLogic {
             return false;
         }
     }
-    
+    /**
+     * Check for empty fields and a valid author
+     * @param s
+     * @return whether or not the suggestion is valid
+     */
     public static boolean isValidSuggestion(Suggestion s) {
         if (s.getSubject().isEmpty()) {
             return false;
@@ -44,7 +55,11 @@ public class SuggestionLogic {
         System.out.println("This is a valid suggestion");
         return true;
     }
-    
+    /**
+     * 
+     * @param s
+     * @return a list of comments found for the suggestion
+     */
     public static ArrayList<Comment> getComments(Suggestion s){
         ArrayList<Comment> comments = new ArrayList<Comment>();
         CommentModel commentModel = new CommentModel();
@@ -56,7 +71,12 @@ public class SuggestionLogic {
         }
         return comments;
     }
-    
+    /**
+     * Add a comment to the suggestion object and save it to Mongo
+     * @param sug
+     * @param commentText
+     * @return whether or not the entry was successfully savd to Mongo
+     */
     public static boolean addComment(Suggestion sug, String commentText) {
         if (commentText.trim().length() > 0) {
             Comment comment = new Comment(commentText, FOTE.getUser().getId());
@@ -68,13 +88,23 @@ public class SuggestionLogic {
             return false;
         }
     }
-
+    /**
+     * 
+     * @param suggestion
+     * @param subject
+     * @param description
+     * @return whether or not the update was successful
+     */
     public static boolean updateSuggestion(Suggestion suggestion, String subject, String description) {
         suggestion.setSubject(subject);
         suggestion.setDescription(description);
         return MongoHelper.save(suggestion, "suggestions");
     }
-    
+    /**
+     * Remove the entry from Mongo only if the user is the owner
+     * @param s
+     * @return whether or not the entry was deleted
+     */
     public static boolean deleteSuggestion(Suggestion s) {
         if (FOTE.getUser().getId() != s.getAuthor()) {
             return false;
